49db1a6fd1b5a38a966b052640f95ad1d592a61d
ram-a-dhan <ram-a-dhan@github.com>
2019-12-25 16:31:43 +0700

    menyelesaikan tugas 5.js

--- part_1/5.js
+++ part_1/5.js
@@ -10,8 +10,51 @@
   // [ 111, 22, 55]
 */
 function palindromeChanger(numbers) {
-  // code below here
+  
+  let arrNum = [], len, wtf = [];
+  
+  for (let i = 0; i < numbers.length; i++) { // loop for each number
+    do {
+      arrNum = numbers[i].toString().split(''); // compart number into digits in an array
+      len = Math.floor(arrNum.length / 2); // half-length of number digits
+      wtf = []; // Whether True or False
+      // console.log(arrNum);
+      // console.log(len);
+      
+      for (let j = 0; j < len; j++) { // loop to check palindromity
+        if (arrNum[j] === arrNum[arrNum.length-1-j]) {
+          wtf.push(true); // if 1 segment has palindrome characteristic, add TRUE to WTF checklist
+        } else {
+          wtf.push(false); // vice versa
+        }
+      }
+      // console.log(wtf);
+
+      if (wtf.indexOf(false) > -1) { // if WTF checklist has at least 1 FALSE then add 1 to number
+        numbers[i]++;
+      }
+    } while (wtf.indexOf(false) > -1) // process loops on until whole number is palindrome (no FALSE in WTF)
+    // console.log(numbers[i]);
+  }
+  return numbers;
 }
 
-console.log(palindromeChanger([117, 9, 20, 30, 42]))
+console.log('');
+
+//// TEST CASE
+var isItPal = [117, 9, 20, 30, 42];
+console.log('Before:');
+console.log(isItPal);
+console.log('After:');
+console.log(palindromeChanger(isItPal));
 // [ 121, 9, 22, 33, 44 ]
+console.log('');
+
+//// CUSTOM TEST CASE
+// var isItPal = [1119, 404309, 2020];
+// console.log('Before:');
+// console.log(isItPal);
+// console.log('After:');
+// console.log(palindromeChanger(isItPal));
+// // [ 1221, 404404, 2112 ]
+// console.log('');
