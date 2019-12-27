2aa4186ec9b333cbcaf0c1127c7dc0c24bae64db
ram-a-dhan <ram-a-dhan@github.com>
2019-12-27 17:36:53 +0700

    menyelesaikan tugas 6.js

--- part_1/6.js
+++ part_1/6.js
@@ -22,10 +22,60 @@ RULES:
  - WAJIB MENYERTAKAN ALGORITMA/PSEUDOCODE
  - DILARANG MENGGUNAKAN REGEX
 */
+
+/*
+ALGORITHM/PSEUDOCODE FOR FUNCTION:
+
+// declare count 0
+// 
+// change all str elements to lower case
+// 
+// FOR 0 <--> str.length-2
+//   IF 3 consecutive str elements === partial
+//     count++
+//   ENDIF
+// ENDFOR
+// 
+// IF count > 0
+//   RETURN count
+// ELSE
+//   RETURN "not found!"
+// ENDIF
+
+*/
 function partialCheck(str, partial) {
-  //your code here    
+  
+  let count = 0;
+
+  // let cc, str2 = '';
+  // for (x = 0; x < str.length; x++) {
+  //   cc = str.charCodeAt(x);
+  //   if (cc >= 65 && cc <= 90) {
+  //     str2 += String.fromCharCode(cc + 32);
+  //   } else {
+  //     str2 += str[x];
+  //   }
+  // }
+  // console.log(str2);
+
+  str = str.toLowerCase(); // the only other way without regexp is above, typed as comments, which unfortunately contains more built-in functions
+
+  for (i = 0; i < str.length-2; i++) {
+    if (str[i] === partial[0] && str[i+1] === partial[1] && str[i+2] === partial[2]) {
+      count++;
+    }
+  }
+
+  if (count > 0) {
+    return `found ${count} times!`
+  } else {
+    return 'not found!'
+  }
 }
+
+console.log();
 console.log(partialCheck('abcdcabdabc', 'abc')); // found 2 times
 console.log(partialCheck('zachariah', 'ach')); // found 1 time
 console.log(partialCheck('accHghebchg', 'chg')); // found 2 times
 console.log(partialCheck('asjekfogiy', 'foy')); // not found
+console.log();
