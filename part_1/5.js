5621d6089aa97e06b5e324cc52f4fa4b598dcbad
ram-a-dhan <58775678+ram-a-dhan@users.noreply.github.com>
2020-01-04 20:56:41 +0700

    Update 5.js

--- part_1/5.js
+++ part_1/5.js
@@ -1,62 +1,58 @@
 /*
   PALINDROME CHANGER
-
   Palindrome changer adalah sebuah fungsi untuk mengubah semua isi
   array of numbers menjadi angka palindrome. Palindrome adalah sebuah kata atau angka
   yang sama nilainya ketika dibalik.
-
   EXAMPLE:
   console.log(palindromeChanger([102, 12, 47]))
   // [ 111, 22, 55]
 */
 function palindromeChanger(numbers) {
   
-  let digits = [], len, wtf = [];
+    let digits = [], len, isNotPal;
+    
+    for (let i = 0; i < numbers.length; i++) { // loop for each number
+      do {
   
-  for (let i = 0; i < numbers.length; i++) { // loop for each number
-    do {
-
-      // arrNum = numbers[i].toString().split(''); // compart number into digits in an array
-      // len = Math.floor(arrNum.length / 2); // half-length of number digits
-      digits = '' + numbers[i];
-      len = digits.length / 2; // half-length of number digits
-      wtf = []; // Whether True or False
-      // console.log(digits);
-      // console.log(len);
-      
-      for (let j = 0; j < len; j++) { // loop to check palindromity
-        if (digits[j] === digits[digits.length-1-j]) {
-          wtf.push(true); // if 1 segment has palindrome characteristic, add TRUE to WTF checklist
-        } else {
-          wtf.push(false); // vice versa
+        digits = '' + numbers[i]; // convert number into string
+        len = digits.length / 2; // half-length of string digits
+        isNotPal = 0; // Non-Palindrome Indicator
+        // console.log(digits);
+        // console.log(len);
+        
+        for (let j = 0; j < len; j++) { // loop to check palindromity
+          if (digits[j] !== digits[digits.length-1-j]) {
+            isNotPal++; // if 1 segment does not have palindrome characteristic, add 1 to isNotPal
+          }
         }
-      }
-      // console.log(wtf);
+        // console.log(isNotPal);
+        
+        if (isNotPal) { // if isNotPal has positive (truthy) number value then add 1 to number
+          numbers[i]++;
+        }
 
-      if (wtf.indexOf(false) > -1) { // if WTF checklist has at least 1 FALSE then add 1 to number
-        numbers[i]++;
-      }
-    } while (wtf.indexOf(false) > -1) // process loops on until whole number is palindrome (no FALSE in WTF)
-    // console.log(numbers[i]);
+      } while (isNotPal) // process loops on until whole number is palindrome (isNotPal === 0)
+
+    }
+    return numbers;
+
   }
-  return numbers;
-}
-
-console.log('');
-
-//// TEST CASE
-var isItPal = [117, 9, 20, 30, 42];
-console.log('Before:');
-console.log(isItPal);
-console.log('After:');
-console.log(palindromeChanger(isItPal));
-// [ 121, 9, 22, 33, 44 ]
-console.log('');
-
-//// CUSTOM TEST CASE
-// var isItPal = [157921, 42609, 3429000, 101100000];
-// console.log('Before:');
-// console.log(isItPal);
-// console.log('After:');
-// console.log(palindromeChanger(isItPal));
-// console.log('');
+  
+  console.log('');
+  
+  //// TEST CASE
+  var isItPal = [117, 9, 20, 30, 42];
+  console.log('Before:');
+  console.log(isItPal);
+  console.log('After:');
+  console.log(palindromeChanger(isItPal));
+  // [ 121, 9, 22, 33, 44 ]
+  console.log('');
+  
+  // CUSTOM TEST CASE
+  var isItPal = [157921, 42609, 3429000, 101100000];
+  console.log('Before:');
+  console.log(isItPal);
+  console.log('After:');
+  console.log(palindromeChanger(isItPal));
+  console.log('');
