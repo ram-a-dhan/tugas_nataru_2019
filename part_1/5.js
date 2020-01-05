b641d85ca3dc9c5877116d05c1a88b8b7907c208
ram.adhan <ram.adhan@outlook.com>
2020-01-05 23:46:17 +0700

    Sedikit mempersingkat kode & menambahkan comments + pseudocode 5.js

--- part_1/5.js
+++ part_1/5.js
@@ -7,32 +7,43 @@
   console.log(palindromeChanger([102, 12, 47]))
   // [ 111, 22, 55]
 */
+
+// // PSEUDOCODE
+// DECLARE numStr, notPal
+// FOR 0 <= a < numbers.length
+//     numStr = "numbers[a]"
+//     DOWHILE notPal > 0
+//         notPal = 0
+//         FOR 0 <= b < numStr.length/2
+//             IF numStr[b] != numStr[numStr.length-1-b]
+//               notPal++
+//               numbers[a]++
+//               numStr = "numbers[a]"
+//             ENDIF
+//         ENDFOR
+//     ENDDOWHILE
+// ENDFOR
+// RETURN numbers
+
 function palindromeChanger(numbers) {
   
-    let digits = [], len, isNotPal;
-    
+    let digits, halfLength, isNotPal;
     for (let i = 0; i < numbers.length; i++) { // loop for each number
       do {
-  
         digits = '' + numbers[i]; // convert number into string
-        len = digits.length / 2; // half-length of string digits
-        isNotPal = 0; // Non-Palindrome Indicator
-        // console.log(digits);
-        // console.log(len);
-        
-        for (let j = 0; j < len; j++) { // loop to check palindromity
-          if (digits[j] !== digits[digits.length-1-j]) {
+        halfLength = digits.length / 2; // half-length of digits string
+        isNotPal = 0; // Non-Palindrome Indicator initial set / reset
+
+        for (let j = 0; j < halfLength; j++) { // looping whole digits string to check palindromity
+          if (digits[j] !== digits[digits.length-1-j]) { // comparing [0] and [end], [1] and [end-1], [2] and [end-2], and so on
             isNotPal++; // if 1 segment does not have palindrome characteristic, add 1 to isNotPal
+            numbers[i]++; // adding 1 to number
+            digits = '' + numbers[i]; // reassign new number to digits string
           }
         }
         // console.log(isNotPal);
-        
-        if (isNotPal) { // if isNotPal has positive (truthy) number value then add 1 to number
-          numbers[i]++;
-        }
-
+        // console.log(numbers[i]);
       } while (isNotPal) // process loops on until whole number is palindrome (isNotPal === 0)
-
     }
     return numbers;
 
@@ -50,7 +61,7 @@ function palindromeChanger(numbers) {
   console.log('');
   
   // CUSTOM TEST CASE
-  var isItPal = [157921, 42609, 3429000, 101100000];
+  var isItPal = [9876543210, 123456789, 9001, 100009];
   console.log('Before:');
   console.log(isItPal);
   console.log('After:');
